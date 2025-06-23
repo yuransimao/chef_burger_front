d114775350d5162da1438970cd071366fe06b292
yuransimao <yuransimao7@gmail.com>
2025-06-23 19:45:20 +0100

    update

--- src/components/inputContainer.tsx
+++ src/components/inputContainer.tsx
@@ -1,19 +1,34 @@
+
 "use client"
 import React from 'react'
 import { Input } from "@/components"
+import { ControllerRenderProps, FieldPath, FieldValues } from "react-hook-form"
 
-interface InputContainerProps{
+
+interface InputContainerProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> {
     title: string;
-    icon : React.ReactNode;
-    type:string;
-    field: unknown
+    icon: React.ReactNode;
+    type: string;
+    field: ControllerRenderProps<TFieldValues, TName>;
 }
-function InputContainer({title, icon,type, field}:InputContainerProps) {
+
+function InputContainer<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({ title, icon, type, field }: InputContainerProps<TFieldValues, TName>) {
     return (
         <div>
-            <div className="flex items-center border-2 rounded-lg  px-4 focus-within:ring focus-within:ring-red-800 transition-colors duration-300 inset-shadow-sm">
+            <div className="flex items-center border-2 rounded-lg px-4 focus-within:ring focus-within:ring-red-800 transition-colors duration-300 inset-shadow-sm">
                 {icon}
-                <Input placeholder={title} type={type} {...field} className="border-0 w-full rounded-none focus-visible:outline-none focus-visible:ring-0" />
+                <Input 
+                    placeholder={title} 
+                    type={type} 
+                    {...field} 
+                    className="border-0 w-full rounded-none focus-visible:outline-none focus-visible:ring-0" 
+                />
             </div>
         </div>
     )
